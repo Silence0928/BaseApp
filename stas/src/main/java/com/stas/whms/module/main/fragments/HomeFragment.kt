7656e5ea74fcf5a407efc4d73dba2c479ebe0f47
zhaohongtao <zhaohongtao@huodada.com>
2023-10-18 10:14:01 +0800

    添加操作权限判断

--- stas/src/main/java/com/stas/whms/module/main/fragments/HomeFragment.kt
+++ stas/src/main/java/com/stas/whms/module/main/fragments/HomeFragment.kt
@@ -1,20 +1,36 @@
 package com.stas.whms.module.main.fragments
 
+import com.alibaba.fastjson.JSON
+import com.hjq.toast.ToastUtils
 import com.lib_common.base.fragment.BaseMvvmFragment
 import com.lib_common.base.mvvm.BaseViewModel
+import com.lib_common.constants.MmkvConstants
 import com.stas.whms.R
+import com.stas.whms.bean.LoginInfo
 import com.stas.whms.databinding.FragmentHomeBinding
 import com.stas.whms.utils.RouteJumpUtil
 
 class HomeFragment: BaseMvvmFragment<FragmentHomeBinding, BaseViewModel>() {
     override fun onViewEvent() {
+        val loginInfoStr = mMMKV.decodeString(MmkvConstants.MMKV_LOGIN_INFO)
+        var level: String? = null
+        if (loginInfoStr != null) {
+            val loginInfo = JSON.parseObject(loginInfoStr, LoginInfo::class.java)
+            if (loginInfo != null) {
+                level = loginInfo.Level
+            }
+        }
         // 入库采集
         mDataBinding.tvStorageCollection.setOnClickListener {
             RouteJumpUtil.jumpToStorageCollection()
         }
         // 入库审核
         mDataBinding.tvStorageAudit.setOnClickListener {
-            RouteJumpUtil.jumpToStorageAudit()
+            if (level == "1") {
+                RouteJumpUtil.jumpToStorageAudit()
+            } else {
+                ToastUtils.show("您暂无此操作权限，请联系班长处理！")
+            }
         }
         // 在库查询
         mDataBinding.tvStockQuery.setOnClickListener {
@@ -30,7 +46,11 @@ class HomeFragment: BaseMvvmFragment<FragmentHomeBinding, BaseViewModel>() {
         }
         // 退库审核
         mDataBinding.tvRefundStockAudit.setOnClickListener {
-            RouteJumpUtil.jumpToRefundAudit()
+            if (level == "1") {
+                RouteJumpUtil.jumpToRefundAudit()
+            } else {
+                ToastUtils.show("您暂无此操作权限，请联系班长处理！")
+            }
         }
     }
 
--- stas/src/main/java/com/stas/whms/module/main/fragments/ShiftParkFragment.kt
+++ stas/src/main/java/com/stas/whms/module/main/fragments/ShiftParkFragment.kt
@@ -1,20 +1,36 @@
 package com.stas.whms.module.main.fragments
 
+import com.alibaba.fastjson.JSON
+import com.hjq.toast.ToastUtils
 import com.lib_common.base.fragment.BaseMvvmFragment
 import com.lib_common.base.mvvm.BaseViewModel
+import com.lib_common.constants.MmkvConstants
 import com.stas.whms.R
+import com.stas.whms.bean.LoginInfo
 import com.stas.whms.databinding.FragmentShiftingParkingBinding
 import com.stas.whms.utils.RouteJumpUtil
 
 class ShiftParkFragment: BaseMvvmFragment<FragmentShiftingParkingBinding, BaseViewModel>() {
     override fun onViewEvent() {
+        val loginInfoStr = mMMKV.decodeString(MmkvConstants.MMKV_LOGIN_INFO)
+        var level: String? = null
+        if (loginInfoStr != null) {
+            val loginInfo = JSON.parseObject(loginInfoStr, LoginInfo::class.java)
+            if (loginInfo != null) {
+                level = loginInfo.Level
+            }
+        }
         // 移库采集
         mDataBinding.llMoveCollection.setOnClickListener {
             RouteJumpUtil.jumpToMoveCollection()
         }
         // 移库审核
         mDataBinding.llMoveAudit.setOnClickListener {
-            RouteJumpUtil.jumpToMoveAudit()
+            if (level == "1") {
+                RouteJumpUtil.jumpToMoveAudit()
+            } else {
+                ToastUtils.show("您暂无此操作权限，请联系班长处理！")
+            }
         }
     }
 
